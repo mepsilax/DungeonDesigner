61ed2bf4bf915a5af0b0c37f34badaf68b074bd1
MepsiLax <ryan@vertigomoon.co.uk>
2013-10-13 09:20:01 +0100

    Initial project setup

---
+++ DungeonCreator/src/OpenGLTest.java
@@ -0,0 +1,32 @@
+import java.awt.Canvas;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+
+public class OpenGLTest extends Canvas{
+	
+	public OpenGLTest(){
+		try{
+			Display.setDisplayMode(new DisplayMode(1920, 1080));
+			Display.setTitle("");
+			Display.setParent(this);
+			Display.create();
+		}catch (LWJGLException e){
+			e.printStackTrace();
+		}
+		
+		while(!Display.isCloseRequested()){
+			Display.update();
+			Display.sync(60);
+			
+		}
+		Display.destroy();
+		
+	}
+	
+	public static void main(String[] args){
+		new OpenGLTest();
+	}
+}
